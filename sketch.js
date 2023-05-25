0cb5f09c9466c5a9409375f8de2d216e01180c3d
semonemo_0825 <51160666+kiase@users.noreply.github.com>
2023-05-26 00:00:59 +0900

    전자기파 발생 완성?

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 let xSpacing = 1; 
-let w; 
-let amplitude = 75; 
-let wavelength = 300; 
-let wavelengthc = 1; 
+let w;
+let el_size = 7.5;
+let wavelengthc = 30;
+let wavelength = 71*5-wavelengthc*5; 
 let frequency = 5;
 let capa_revarse = 0;
 let period = 1/frequency;
@@ -12,10 +12,14 @@ let frequencyc = 1;
 let P_gukpangap;
 let mode_choice;
 let coil_onoff;
+let rotated = 0;
 let leng = 50;
-let tum = 80;
+let tum = 60;
 let time = 0; 
+let amplitude = ((leng*tum)*(el_size/5))/100; 
 let mode = true;
+let reset_but;
+let see_opt = [true, true];
 
 let capacitorX = -400; // 축전기의 시작 x 위치
 let capacitorY = -50; // 축전기의 시작 y 위치
@@ -23,6 +27,10 @@ let capacitorY = -50; // 축전기의 시작 y 위치
 function preload() {
   myFont = loadFont('assets/inconsolata.otf');
   kfont = loadFont('assets/NanumSquareB.otf');
+}
+
+function mapRange(value, min1, max1, min2, max2) {
+  return ((value - min1) / (max1 - min1)) * (max2 - min2) + min2;
 }
 
 function setup() {
@@ -34,23 +42,32 @@ function setup() {
   mode_choice.option('수신 (미완성)');
   mode_choice.selected('송신').changed(updateMode);
   
-  createSlider(1, 60, wavelengthc, 1).position(10, height + 70).input(updateHz);
-  P_hz = createP(`전류 주파수 : ${wavelengthc}Hz`).position(10, height + 80).style('color','#FFFFFF');
-
-  createSlider(0, 100, amplitude).position(10, height + 10).input(updateAmplitude);
-  P_amplitude = createP(`전류 크기 : ${amplitude/20}A`).position(10, height + 20).style('color','#FFFFFF');
+  reset_but = createButton('초기화').position(410, height + 70).size(100, 40);
+  reset_but.mousePressed(re_f);
   
-  capa_onoff = createCheckbox('축전기 활성화', false).position(210, height + 70).style('color','#FFFFFF');
+  createSlider(1, 60, wavelengthc, 1).position(10, height + 70).input(updateHz);
+  P_hz = createP(`교류 진동수 : ${wavelengthc}Hz`).position(10, height + 80).style('color','#FFFFFF');
+
+  createSlider(0, 10, el_size, 0.5).position(10, height + 10).input(updateAmplitude);
+  P_amplitude = createP(`전류 크기 : ${el_size}A`).position(10, height + 20).style('color','#FFFFFF');
+  
+  capa_onoff = createCheckbox('축전기 보이기', true).position(210, height + 70).style('color','#FFFFFF');
   capa_onoff.changed(capa_trigger);
+  
+  wave_onoff = createCheckbox('전자기파 보이기', true).position(210, height + 100).style('color','#FFFFFF');
+  wave_onoff.changed(wave_trigger);
 
   createSlider(20, 100, leng, 5).position(210, height + 10).input(updateGukpanleng);
-  P_gukpanleng = createP(`극판 면적(미적용) : ${leng}`).position(210, height + 20).style('color','#999999');
+  P_gukpanleng = createP(`극판 면적 : ${leng}`).position(210, height + 20).style('color','#FFFFFF');
 
-  createSlider(10, 100, tum).position(410, height + 10).input(updateGgukpangap);
-  P_gukpangap = createP(`극판 사이 간격(미적용) : ${110-tum}`).position(410, height + 20).style('color','#999999');
+  createSlider(10, 90, tum, 5).position(410, height + 10).input(updateGgukpangap);
+  P_gukpangap = createP(`극판 사이 간격 : ${110-tum}`).position(410, height + 20).style('color','#FFFFFF');
 
-  createSlider(0.0, 10, frequency, 2.5).position(700, height + 10).input(updateFrequency).attribute('di1sabled', 'true');
+  createSlider(0.0, 20, frequency, 2.5).position(700, height + 10).input(updateFrequency).attribute('dlisabled', 'true');
   P_speed = createP(`속도 배수 : ${frequencyc}배`).position(700, height + 20).style('color','#FFFFFF');
+  
+  createSlider(-100, 80, rotated, 5).position(700, height + 70).input(updateRotate).attribute('dlisabled', 'true');
+  P_rotate = createP(`그래프 회전(X축) : ${rotated+10}도 (기본)`).position(700, height + 80).style('color','#FFFFFF');
 }
 
 function draw() {
@@ -63,17 +80,21 @@ function draw() {
   text('● : 자기장', -440, -160);
   fill('red');
   text('● : 전기장', -440, -140);
+  fill('gray');
+  text('보기 쉽도록 하기 위해 수치가 조정되어 있습니다.', 140, -180);
   // 축전기 그리기
-  if((time+40)*2-(wavelength*capa_revarse*2) < -wavelength) {
-    capa_revarse -= 1;
-  }
-  if((time+40)*2-(wavelength*capa_revarse*2) <= wavelength) {
-        drawCapacitor(leng, tum, true);
-      } else {
-        drawCapacitor(leng, tum, false);
-      }
-  if((time+40)*2-(wavelength*capa_revarse*2) >= wavelength*2) {
-    capa_revarse += 1;
+  if(see_opt[0]) {
+    if((time+40)*2-(wavelength*capa_revarse*2) < -wavelength) {
+      capa_revarse -= 1;
+    }
+    if((time+40)*2-(wavelength*capa_revarse*2) <= wavelength) {
+          drawCapacitor(leng, tum, true);
+        } else {
+          drawCapacitor(leng, tum, false);
+        }
+    if((time+40)*2-(wavelength*capa_revarse*2) >= wavelength*2) {
+      capa_revarse += 1;
+    }
   }
   let x = -time;
   let y = 0; 
@@ -81,41 +102,43 @@ function draw() {
   let startX = w+100; 
   let startY = 50; 
   let startZ = 0; 
-  rotateY(radians(-20)); 
+  rotateY(radians(-20));
+  rotateX(radians(-rotated));
   let ypos = 0;
-  stroke(255, 0, 0); 
-  strokeWeight(2);
-  noFill();
-  beginShape();
-  vertex(startX, startY, startZ);
+  if(see_opt[1]) {
+    stroke(255, 0, 0); 
+    strokeWeight(2);
+    noFill();
+    beginShape();
+    vertex(startX, startY, startZ);
+    for (let i = 0; i < w / xSpacing; i++) {
+      x -= xSpacing;
+      let posX = startX - i * xSpacing;
+      y = amplitude * sin((x / wavelength) * TWO_PI - HALF_PI);
+      let posY = y;
+      if(i==0){
+        ypos = y;
+      }
 
-  for (let i = 0; i < w / xSpacing; i++) {
-    x -= xSpacing;
-    let posX = startX - i * xSpacing;
-    y = amplitude * sin((x / wavelength) * TWO_PI - HALF_PI);
-    let posY = y;
-    if(i==0){
-      ypos = y;
+      vertex(posX-200, posY+50, z);
     }
-    
-    vertex(posX-200, posY+50, z);
+    endShape();
+    x = -time;
+    y = 0;
+    stroke(0, 0, 255); 
+    strokeWeight(2);
+    beginShape();
+    vertex(startX, startY, startZ);
+    for (let i = 0; i < w / xSpacing; i++) {
+      x -= xSpacing;
+      let posX = startX - i * xSpacing;
+      z = amplitude * sin((x / wavelength) * TWO_PI - HALF_PI);
+      let posZ = z*-1;
+      vertex(posX-200, y+50, posZ);
+    }
+    endShape();
+
   }
-  endShape();
-  x = -time;
-  y = 0;
-  stroke(0, 0, 255); 
-  strokeWeight(2);
-  beginShape();
-  vertex(startX, startY, startZ);
-  for (let i = 0; i < w / xSpacing; i++) {
-    x -= xSpacing;
-    let posX = startX - i * xSpacing;
-    z = amplitude * sin((x / wavelength) * TWO_PI - HALF_PI);
-    let posZ = z*-1;
-    vertex(posX-200, y+50, posZ);
-  }
-  endShape();
-  
   time += frequency;
   
 }
@@ -124,34 +147,54 @@ function updateMode() {
   
 }
 
-function coil_trigger() {
+function wave_trigger() {
+  if (this.checked()) {
+    see_opt[1] = true;
+  } else {
+    see_opt[1] = false;
+  }
   
 }
 
 function capa_trigger() {
-  
+  if (this.checked()) {
+    see_opt[0] = true;
+  } else {
+    see_opt[0] = false;
+  }
 }
 
 function updateAmplitude() {
-  amplitude = this.value();
-  P_amplitude.html(`전류 크기 : ${amplitude/10}A`);
-  
+  el_size = this.value();
+  amplitude = ((leng*tum)*(el_size/5))/100;
+  P_amplitude.html(`전류 크기 : ${el_size}A`);
 }
 
 function updateHz() {
   wavelength = 71*5-this.value()*5;
   wavelengthc = this.value();
-  P_hz.html(`전류 주파수 : ${wavelengthc}Hz`);
+  P_hz.html(`교류 진동수 : ${wavelengthc}Hz`);
 }
 
 function updateGukpanleng() {
   leng = this.value();
-  P_gukpanleng.html(`극판 면적(미적용) : ${leng}`);
+  amplitude = ((leng*tum)*(el_size/5))/100;
+  P_gukpanleng.html(`극판 면적 : ${leng}`);
 }
 
 function updateGgukpangap() {
   tum = this.value();
-  P_gukpangap.html(`극판 사이 간격(미적용) : ${110-tum}`);
+  amplitude = ((leng*tum)*(el_size/5))/100;
+  P_gukpangap.html(`극판 사이 간격 : ${110-tum}`);
+}
+
+function updateRotate() {
+  rotated = this.value();
+  if(this.value() == 0){
+    P_rotate.html(`그래프 회전(X축) : ${rotated+10}도 (기본)`);
+  } else {
+    P_rotate.html(`그래프 회전(X축) : ${rotated+10}도`);
+  }
 }
 
 function updateFrequency() {
@@ -159,6 +202,10 @@ function updateFrequency() {
   frequencyc = this.value()/5;
   P_speed.html(`속도 배수 : ${frequencyc}배`);
 }
+  
+function re_f(){
+    window.location.reload();
+  } 
 
 // 축전기 그리기
 function drawCapacitor(size, tum, revarse) {
@@ -194,8 +241,21 @@ function drawCapacitor(size, tum, revarse) {
     text("+ ".repeat(size/13), capacitorX+170, capacitorY+7+tum/2);
     fill('red');
     text("- ".repeat(size/13), capacitorX+170, capacitorY+47+tum/2+(110-tum));
-    textAlign(LEFT, BASELINE);
+    for(let i =0;i<size/11;i++){
+      rect(capacitorX+170-size+i*20, capacitorY+25+tum/2, 5, (110-tum));
+      triangle(capacitorX+165-size+i*20, capacitorY+22+tum/2+(110-tum), capacitorX+180-size+i*20, capacitorY+22+tum/2+(110-tum), capacitorX+173-size+i*20, capacitorY+35+tum/2+(110-tum));
+    }
+    fill('blue');
+    textSize(30);
+    text('<', capacitorX+163, capacitorY+87);
+    text('<', capacitorX+163-size/2, capacitorY+87);
+    text('<', capacitorX+163+size/2, capacitorY+87);
+    stroke('blue');
+    noFill();
+    arc(capacitorX+163, capacitorY+78, size*2+20, 25, 0, PI);
+    noStroke();
   } else {
+    noStroke();
     textSize(60);
     fill('orange');
     text("<---", capacitorX+25, capacitorY-13.5);
@@ -204,8 +264,20 @@ function drawCapacitor(size, tum, revarse) {
     textSize(30);
     textAlign(CENTER, CENTER);
     text("- ".repeat(size/13), capacitorX+170, capacitorY+7+tum/2);
+    for(let i =0;i<size/11;i++){
+      rect(capacitorX+170-size+i*20, capacitorY+33+tum/2, 5, (110-tum));
+      triangle(capacitorX+165-size+i*20, capacitorY+38+tum/2, capacitorX+180-size+i*20, capacitorY+38+tum/2, capacitorX+173-size+i*20, capacitorY+25+tum/2);
+    }
     fill('blue');
     text("+ ".repeat(size/13), capacitorX+170, capacitorY+47+tum/2+(110-tum));
-    textAlign(LEFT, BASELINE);
+    textSize(30);
+    text('>', capacitorX+163, capacitorY+87);
+    text('>', capacitorX+163-size/2, capacitorY+87);
+    text('>', capacitorX+163+size/2, capacitorY+87);
+    stroke('blue');
+    noFill();
+    arc(capacitorX+163, capacitorY+78, size*2+20, 25, 0, PI);
+    
   }
+  textAlign(LEFT, BASELINE);
 }
